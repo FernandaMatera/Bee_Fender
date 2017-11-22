e7330422338ddf63fb5bdb850feba7309e9ef1cb
FernandaMatera <fernanda.matera.a@gmail.com>
2017-11-21 23:26:23 -0300

    02_21-11-2017
    Traspaso de escenas

---
+++ Assets/Scripts/Boton_Panal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boton_Panal : MonoBehaviour {
+
+    public Animator controlador;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void boton ()
+    {
+        controlador.enabled = true;
+        bool estadoActual = controlador.GetBool("abajo");
+        controlador.SetBool("abajo", !estadoActual);
+    }
+}
